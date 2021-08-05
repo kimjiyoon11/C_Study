964f9569a9c5d1e8445f8100ee62edf2ad8b6662
kimjiyoon11 <78461985+kimjiyoon11@users.noreply.github.com>
2021-08-05 15:57:19 +0900

    C 구조체 생성 예제
    구조체 생성 예시

--- C_Study/C_Struct.c
+++ C_Study/C_Struct.c
@@ -24,15 +24,15 @@ int main(void){
 		
 	printf("======= person B =======\n");
 	
-	struct person A;
+	struct person B;
 	
-	B = A;
+	B = A; //구조체 B에 A를 대입
 	
-	printf("name : %s\nage : %d\nheight : %d\nweight : %f\n", B.name, B.age, B.height, B.weight);
+	printf("name : %s\nage : %d\nheight : %d\nweight : %f\n", B.name, B.age, B.height, B.weight); //A에 대한 데이터들 출력
 	
 	printf("======= person C =======\n");
 	
-	struct person C = {"B_jiyoon", 25, 180, 70};
+	struct person C = {"B_jiyoon", 25, 180, 70}; //구조체를 생성하면서 값을 넣어줌
 	
 	printf("name : %s\nage : %d\nheight : %d\nweight : %f\n", C.name, C.age, C.height, C.weight);
 	
