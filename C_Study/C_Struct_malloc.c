ab062c1becf43e8eb46271a5cdb52f34f1f7c081
kimjiyoon11 <78461985+kimjiyoon11@users.noreply.github.com>
2021-08-05 16:02:59 +0900

    구조체 동적 메모리 할당

--- C_Study/C_Struct_malloc.c
+++ C_Study/C_Struct_malloc.c
@@ -20,33 +20,33 @@ int main (void){
 	
 	user *pUser;
 	
-	printf("Number of data to input : ");
-	scanf("%d", &nNum);
+	printf("Number of data to input : "); 
+	scanf("%d", &nNum); //입력할 데이터 수
 	printf("\n");
 
-	pUser = (user *)malloc(nNum * sizeof(user));
+	pUser = (user *)malloc(nNum * sizeof(user)); //pUser 포인터에 (USER 구조체 크기 * 입력할 데이터 수) 만큼의 메모리 할당
 	//pUser = (user *)malloc(size * sizeof(user));
 	
-	if(pUser == NULL)
+	if(pUser == NULL) //pUser 포인터에 메모리 할당 실패 시 종료
 	{
 		printf("malloc fail");
 		exit(1);
 	}		
 	
-	for(int i = 0; i < nNum; i++)
+	for(int i = 0; i < nNum; i++) //데이터 입력 for문
 	{
 		printf("============ %d : input data ============\n", i);
 		
 		(pUser + i)->index = i;
 		
 		printf("name : ");
-		scanf("%s",(pUser + i)->name);
+		scanf("%s",(pUser + i)->name); 
 		
 		printf("age : ");
 		scanf("%d", &(pUser + i)->age);
 	}
 	
-	printUser(pUser, nNum);
+	printUser(pUser, nNum); //출력함수
 	free(pUser);
 	return 0;
 }
